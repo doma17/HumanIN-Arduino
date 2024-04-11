6c0a96c22ea61e4011ee6f52874aa2677a3c79e9
doma17 <rovin1273@gmail.com>
2024-04-11 21:16:17 +0900

    update code in 2024.4.11

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -2,88 +2,150 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 #define RELAY_PIN 5
+#define LED_PIN 4  // 내장 LED 핀 번호 정의
 
-const char* ssid = "G-BRAIN";
-const char* password = "gbrain0814";
-const char* mqttServer = "61.101.55.248";
-const int mqttPort = 1883;
 
-// receiver Serial Code를 넣어주세요! ex) gbrain_GH8
-const char* mqttClientID = "deviceSerialCode";
-const char* mqttUsername = "gbrain";
-const char* mqttPassword = "gbrain";
+/// !!! -> Need to be changed for each device
+/// /// -> No action
 
-const String baseMqttTopic = "gbrain";
+const char* ssid = "G-BRAIN_GH7";         /// !!! KYU IL LEE: WiFi from the receiver
+const char* password = "gbrain0814";      /// /// KYU IL LEE: Password for access to WiFi from the receiver
+const char* mqttServer = "61.101.55.94";  /// /// KYU IL LEE: Gbrain Company IP from Internet Provider (Can be changed)
+const int mqttPort = 1883;                /// /// KYU IL LEE: MQTT port (default / no change)
+
+// receiver Serial Code를 넣어주세요! eg) gbrain_GH8
+const char* mqttClientID = "GH7_receiver";  /// !!! KYU IL LEE: MQTT Client ID for the receiver (GH8: MQTT Client ID for the app on the mobile)
+const char* mqttUsername = "gbrain";        /// /// KYU IL LEE: User ID set up in the MQTT server (no change)
+const char* mqttPassword = "gbrain";        /// /// KYU IL LEE: User Password for User ID (no change)
+
+const String topic = "GH7";             /// !!! KYU IL LEE: usage? maybe "topic" name
+const String baseMqttTopic = "gbrain";  /// /// KYU IL LEE: Keep as it is (usage?)
+const char* MQTTTopic = "gbrain/GH7";   /// !!! KYU IL LEE: Added for MQTT subscription
+
+/// /// (Paragraph 1)
+/// /// After uploading the code to the receiver Arduino board,
+/// /// the user needs to connect to the WiFi network (from the receiver) whose ssid (eg. G-BRAIN_GH8) is shown in the above
+/// /// with the password from user's mobile device.
+/// ///
+/// /// (Paragraph 2)
+/// /// After connected to the WiFi from the receiver,
+/// /// the user needs to connect to 192.168.4.1 (browser)
+/// /// and put in user's WiFi ssid (eg. Gbrain) and password,
+/// /// and the topic name (eg. GH8).
+/// /// Click the save button and the receiver board will start to operate.
+/// ///
+/// /// Paragraph 2 is required to be repeated for every turn-on of the receiver board.
+/// ///
+/// /// (Paragraph 3)
+/// /// The user (mobile device) needs to disconnect from the WiFi from the receiver
+/// /// and connect to the user's own WiFi.
+/// /// Then, open the HumanIn App. and choose the working bluetooth device (sender, eg. GH8).
+/// /// The sender will start to work to publish the signal to the MQTT broker,
+/// /// and the receiver will receive (subscribe) the published signal to control TENS.
 
 unsigned long timeVal;
 int check = 0;
+
+// 연결 시간 초과 변수
+unsigned long timeOutVal = 15000;
 
 ESP8266WebServer server(80);
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void handleRoot() {
-  String html = "<html><body>";
-  html += "<h1>Wi-Fi Setting</h1>";
-  html += "<form action=\"/save\" method=\"POST\">";
-  html += "SSID: <input type=\"text\" name=\"ssid\"><br>";
-  html += "Password: <input type=\"password\" name=\"password\"><br>";
-  html += "Topic: <input type=\"text\" name=\"topic\"><br>";
-  html += "<input type=\"submit\" value=\"Save\">";
-  html += "</form>";
-  html += "</body></html>";
+void handleWiFiConnect() {
+  DynamicJsonDocument jsonBuffer(1024);
+  deserializeJson(jsonBuffer, server.arg("plain"));
 
-  server.send(200, "text/html", html);
-}
-
-void handleSave() {
-  String ssid = server.arg("ssid");
-  String password = server.arg("password");
-  String topic = server.arg("topic");
-
-  // 와이파이 정보를 저장하고 설정을 적용하는 로직을 추가하면 됩니다.
-  // 예: WiFi.begin(), while(WiFi.status() != WL_CONNECTED) 등
-
-  String html = "<html><body>";
-  html += "<h1>Wi-Fi Setting Info</h1>";
-  html += "<p>SSID: " + ssid + "</p>";
-  html += "<p>Password: " + password + "</p>";
-  html += "<p>Topic: " + topic + "</p>";
-  html += "</body></html>";
+  String ssid = jsonBuffer["ssid"].as<String>();
+  String password = jsonBuffer["password"].as<String>();
+  String topic = jsonBuffer["topic"].as<String>();
   String result = baseMqttTopic + "/" + topic;
-  
-  char mqttTopic[1000];
-  strcpy(mqttTopic, result.c_str());
 
-  server.send(200, "text/html", html);
+  // char mqttTopic[1000];
+  // strcpy(mqttTopic, result.c_str());
+  const char* mqttTopic = result.c_str();
+  Serial.print("[handleWiFiConnect] SSID = ");
+  Serial.println(ssid);
+  Serial.print("[handleWiFiConnect] Password = ");
+  Serial.println(password);
+  Serial.print("[handleWiFiConnect] MqttTopic = ");
+  Serial.println(mqttTopic);
 
   WiFi.begin(ssid, password);
+
+  unsigned long wifiConnectTimeVal = millis();
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    if (millis() - wifiConnectTimeVal > timeOutVal) {
+      Serial.println("[handleWiFiConnect] Not Connect WIFI. Please check ssid or password");
+      server.send(400, "text/plain", "Not Connect WIFI. Please check ssid or password");
+      return;
+    }
+
+    blinkLED(1000);
+
     Serial.println("Connecting to WiFi...");
   }
-  Serial.println("Connected to WiFi");
 
+  Serial.println("Connected to WiFi");
   client.setServer(mqttServer, mqttPort);
+
+  unsigned long mqttConnectTimeVal = millis();
+
   while (!client.connected()) {
-    Serial.println("Connecting to MQTT Broker...");
+    Serial.println("[handleWiFiConnect] Connecting to MQTT Broker...");
+
     if (client.connect(mqttClientID, mqttUsername, mqttPassword)) {
       Serial.println("Connected to MQTT Broker");
+      Serial.println(mqttClientID);
+      Serial.println(mqttUsername);
+      Serial.println(client.connected());
     } else {
+      if (millis() - mqttConnectTimeVal > timeOutVal) {
+        Serial.println("Not Connect MQTT Server. Please check receiver code");
+        server.send(500, "text/plain", "Not Connect MQTT Server. Please check receiver code");
+        return;
+      }
+
       Serial.print("Failed, rc=");
       Serial.print(client.state());
       Serial.println(" Retrying in 5 seconds...");
-      delay(5000);
+
+      blinkLED(5000);
     }
   }
-
 
 
   // MQTT 메시지 수신 콜백 함수 등록
   client.setCallback(callback);
   // 주제에 대해 구독 시작
+  // client.subscribe(mqttTopic);
+
+  /// /// KYU IL LEE: Changed for variable type matching
+  // client.subscribe(MQTTTopic);
   client.subscribe(mqttTopic);
+
+  server.send(200, "text/plain", "Connect Success!!");
+}
+
+// 입력한 Time 만큼 Delay되면서 LED 점등
+void blinkLED(int time) {
+  int delayTime = time / 2;
+
+  digitalWrite(LED_PIN, HIGH);
+  delay(delayTime);
+  digitalWrite(LED_PIN, LOW);
+  delay(delayTime);
+}
+
+void handleRestart() {
+  Serial.println("restart...");
+  server.send(200, "text/plain", "restart!!");
+
+  ESP.restart();
 }
 
 void setup() {
@@ -97,18 +159,24 @@ void setup() {
   Serial.print("AP IP address: ");
   Serial.println(ipAddress);
 
-  // 웹 서버 설정
-  server.on("/", handleRoot);
-  server.on("/save", handleSave);
+  // 서버 설정
+  server.on("/api/ssid", HTTP_POST, handleWiFiConnect);
+  server.on("/api/restart", HTTP_GET, handleRestart);
 
   server.begin();
-  Serial.println("HTTP server started");
+  Serial.println("API server started");
 
-  
   pinMode(RELAY_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  // LED 켜기
+  digitalWrite(LED_PIN, HIGH);
 }
 
 void loop() {
+  // LED 켜기
+  digitalWrite(LED_PIN, HIGH);
+
   server.handleClient();
   client.loop();
 }
@@ -120,14 +188,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
-  
+
   Serial.println();
-  if ((char)payload[0] == '1' && check == 0){
+  if ((char)payload[0] == '1' && check == 0) {
     timeVal = millis();
     check = 1;
     digitalWrite(RELAY_PIN, HIGH);
-  }
-  else {
+  } else {
     check = 0;
     digitalWrite(RELAY_PIN, LOW);
   }
--- sender/sender.ino
+++ sender/sender.ino
@@ -7,6 +7,7 @@ using namespace std;
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define WINDOW_SIZE 10
 
 const String KEYWORD_PLOT = "plot"; 
 BLEServer *pServer = NULL;
@@ -19,7 +20,11 @@ int sensorValue;
 int check = 0;
 unsigned long timeVal;
 String message = "";
-String deviceName = "TEST";
+String deviceName = "GH7"; // 블루투스 이름
+
+double values[WINDOW_SIZE]; // 센서 값들을 저장할 배열
+int valueIndex = 0; // 현재 값의 인덱스
+double total = 0; // 총합 계산
 
 void configSensor() {
   pinMode(BUILTIN_LED, OUTPUT);
@@ -33,20 +38,36 @@ void writeStrValue(String a) {
 }
 
 void readDataStream() {
+  // 새로운 데이터 포인트 읽기
   doubleValue = adc1_get_raw(ADC1_CHANNEL_4);
   doubleValue = doubleValue*3.3/4095*100;
-  Serial.println(doubleValue);
-  if (doubleValue >= limit && check == 0){
+
+  // 이동 평균 필터 업데이트
+  total -= values[valueIndex]; // 이전 값을 총합에서 빼기
+  values[valueIndex] = doubleValue; // 새로운 값 저장
+  total += values[valueIndex]; // 새로운 값을 총합에 더하기
+  valueIndex = (valueIndex + 1) % WINDOW_SIZE; // 인덱스 업데이트
+
+  double averageValue = total / WINDOW_SIZE; // 이동 평균 계산
+  Serial.println(averageValue);
+
+  // 나머지 코드는 동일하게 사용
+
+  // doubleValue = adc1_get_raw(ADC1_CHANNEL_4);
+  // doubleValue = doubleValue*3.3/4095*100;
+
+  // Serial.println(doubleValue);
+  if (averageValue >= limit && check == 0){
     check = 1;
-    writeStrValue(String(doubleValue) + ",1");
+    writeStrValue(String(averageValue) + ",1");
     timeVal = millis();
   }
   else if (check == 1 && millis() - timeVal >= 1500){
     check = 0;
-    writeStrValue(String(doubleValue) + ",0");
+    writeStrValue(String(averageValue) + ",0");
   }
   else{
-    writeStrValue(String(doubleValue) + ",2");
+    writeStrValue(String(averageValue) + ",2");
   }
 }
 
